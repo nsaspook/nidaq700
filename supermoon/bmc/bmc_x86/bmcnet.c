c160ff6fb0f00e2cbcfce960578fe0b2762d31bf
Fred Brooks <nsaspook@nsaspook.com>
2015-09-05 20:33:26 -0700

    Source add:  Software update and cleanup

--- supermoon/bmc/bmc_x86/bmcnet.c
+++ supermoon/bmc/bmc_x86/bmcnet.c
@@ -8,40 +8,40 @@
 #include<arpa/inet.h> //inet_addr
 #include "bmcnet.h"
 
-extern	char	hostip;
-extern	int	hostport;
+extern char hostip;
+extern int hostport;
 
 int bmc_client(char * net_message)
 {
 	int sock, n;
 	struct sockaddr_in server;
-	char message[1000] , server_reply[2000];
-	char MDB=0;	// debug stuff
+	char message[1000], server_reply[2000];
+	char MDB = 0; // debug stuff
 
 	//Create socket
-	sock = socket(AF_INET , SOCK_STREAM , 0);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == -1) {
 		printf("Could not create socket");
 	}
 
-	server.sin_addr.s_addr = inet_addr((char*)&hostip);
+	server.sin_addr.s_addr = inet_addr((char*) &hostip);
 	server.sin_family = AF_INET;
 	server.sin_port = htons(hostport);
 
 	//Connect to remote server
-	if (connect(sock , (struct sockaddr *) &server , sizeof(server)) < 0) {
+	if (connect(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
 		perror("connect failed. Error");
 		return 1;
 	}
 
 	//Send some data
-	if ( send(sock , net_message , strlen(net_message) , 0) < 0) {
+	if (send(sock, net_message, strlen(net_message), 0) < 0) {
 		puts("Send failed");
 		return 1;
 	}
 
 	//Receive a reply from the server
-	if ( (n = recv(sock , server_reply , 2000 , 0)) < 0) {
+	if ((n = recv(sock, server_reply, 2000, 0)) < 0) {
 		puts("recv failed");
 		close(sock);
 		return 1;
