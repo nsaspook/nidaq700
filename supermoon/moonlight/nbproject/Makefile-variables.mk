1343786106d9a505856f2066ae7907e22c97b59a
Fred Brooks <nsaspook@nsaspook.com>
2015-09-08 19:52:03 -0700

    source update

--- supermoon/moonlight/nbproject/Makefile-variables.mk
+++ supermoon/moonlight/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=None-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/None-Linux-x86
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
 CND_ARTIFACT_NAME_Debug=moonlight
-CND_ARTIFACT_PATH_Debug=dist/Debug/None-Linux-x86/moonlight
-CND_PACKAGE_DIR_Debug=dist/Debug/None-Linux-x86/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/moonlight
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=moonlight.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/None-Linux-x86/package/moonlight.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/moonlight.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
